d6ca858648d82825a930ce5bec046528f1e8361c
IgorMushk <igor.mushketik@gmail.com>
2023-09-12 16:25:45 +0300

    working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,66 @@
-// import React, { Component } from 'react'
+import React, { Component } from 'react'
+import initialTodos from './todos.json';
+import TodoList from './TodoList';
 
-// export default class App extends Component {
-//   render() {
-//     return (
-//       <div>App</div>
-//     )
-//   } 
-// }
+export class App extends Component {
+  state = {
+    todos : initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
 
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!!
-    </div>
-  );
-};
+  render() {
+    const {todos} = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodoCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0,
+    );
+
+    return (
+      <>
+      <h1>Состояние компонента</h1>
+     
+      <div>
+        <p>Общее кол-во: {totalTodoCount}</p>
+        <p>Кол-во выполненных: {completedTodoCount}</p>
+      </div>
+
+      <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+    </>
+    )
+  } 
+}
+
+//export default App
+
+// [
+//   { "id": "id-1", "text": "Выучить основы React", "completed": true },
+//   { "id": "id-2", "text": "Разобраться с React Router", "completed": false },
+//   { "id": "id-3", "text": "Пережить Redux", "completed": false }
+// ]
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template!!
+//     </div>
+//   );
+// };
 
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import './TodoList.css'
 
-export default function TodoList({todos, onDeleteTodo}) {
+const TodoList =({todos, onDeleteTodo}) =>{
   return (
-    <div>TodoList</div>
+    <ul className="TodoList">
+    {todos.map(({ id, text }) => (
+      <li key={id} className="TodoList__item">
+        <p className="TodoList__text">{text}</p>
+        <button onClick={() => onDeleteTodo(id)}>Удалить</button>
+      </li>
+    ))}
+  </ul>
+
   )
 }
+
+
+export default TodoList
